99fd4859bf9f551c95bcee5e0cc1a9ba9da53c0e
TsT <tst2005@gmail.com>
2016-09-04 18:54:36 +0200

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+http://stackoverflow.com/questions/39211316/how-can-i-deep-compare-2-lua-tables-with-cyclic-references-where-keys-of-a-tabl
--- test.tablecompare.lua
+++ test.tablecompare.lua
@@ -1,7 +1,31 @@
 local table_eq = require "tablecompare"
 
-t1={}
-t2={}
-print(
-table_eq({a={x={t1,t2}}, b=t2}, {a={x={t1,t2}}, b=t2})
-)
+local t1={} local t2={}
+assert( table_eq({a={x={t1,t2}}, b=t2}, {a={x={t1,t2}}, b=t2}) )
+
+local t1 = {}
+
+t1[t1]=t1
+t1.x = {[t1] = {1, 2, 3}}
+
+local t2 = {}
+local t3 = {}
+
+t2[t3]=t2
+t3[t2]=t3
+t2.x = {[t3] = {1, 2, 3}}
+t3.x = {[t2] = {1, 2, 3}}
+
+assert(table_eq(t1, t2) == false)
+assert(table_eq(t2, t3) == true)
+
+local t4 = {}
+t4[{}] = 1
+t4["1"] = {}
+
+local t5 = {}
+local t6 = {}
+t5[t6] = 1
+t5["1"] = t6
+
+assert(table_eq(t4, t5) == false)
